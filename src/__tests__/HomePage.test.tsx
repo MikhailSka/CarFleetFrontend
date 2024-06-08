15deb96e6e06636a68122342241e34f534478674
Skarakhodau Mikhail <75039170+MikhailSka@users.noreply.github.com>
2024-06-08 18:37:49 +0200

    tests 3 !!!!!!!!!!!!!!!

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import HomePage from 'pages/user/HomePage'
-
+import HomePage from 'pages/HomePage';
 
 test('renders correctly and matches snapshot', () => {
   const { asFragment } = render(<HomePage />);
